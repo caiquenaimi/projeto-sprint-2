a28d5fafcc4fcd65977705ca1748f5eadc42567b
Caique <caiquenaimi.564@gmail.com>
2023-03-16 17:36:17 -0300

    coments contato

--- pages/main.js
+++ pages/main.js
@@ -1,7 +1,9 @@
+/* constante da janela modal */
 function openModal(){
     const modal = document.getElementById('modal-container')
     modal.classList.add('mostrar')
-  
+ 
+/* configuração para abrir e fechar janela modal */    
     modal.addEventListener('click', (e) =>{
         if (e.target.id == 'modal-container' || e.target.id == "fechar"){
             modal.classList.remove('mostrar')
